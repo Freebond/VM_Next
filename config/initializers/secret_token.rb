684c94124e02775ab805c822d1252cb9e0431503
Massimo Bondanza <massimo.bondanza@gmail.com>
2011-04-19 20:10:06 +0200

    Commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VMNext::Application.config.secret_token = 'a707d998730adce0d747533e7e9ccbee6f15f2c32def9f8f96b6d31a5283e87ca2a83520087720f32e0f51d62fa5478f32f92df14b098723d6ad22e319f31d3a'
